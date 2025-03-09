62752f72c44fce7eef884429e738553dd69cf7cb
Alexander <Usolt>
2025-03-09 13:57:04 +0700

    Initial Commit

---
+++ FirstConsolApp/func.swift
@@ -0,0 +1,11 @@
+//
+//  func.swift
+//  FirstConsolApp
+//
+//  Created by Александр Усольцев on 09.03.2025.
+//
+
+// Задаем функцию, которая принимает два числа и возвращает их сумму
+func sum(first: Int, second: Int) -> Int {
+    return first + second
+}
---
+++ FirstConsolApp/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  FirstConsolApp
+//
+//  Created by Александр Усольцев on 09.03.2025.
+//
+
+import Foundation
+
+
+import Foundation
+
+// Выводим текст о вводе значения
+print("Enter first number")
+
+// Создаем переменную, принимающую значение
+var a = Int(readLine() ?? "") ?? 0
+
+// Выводим запрос для второй переменной
+print("Enter second number")
+
+// Принимаем значение для второй переменной
+var b = Int(readLine() ?? "") ?? 0 // Исправлено readline() на readLine()
+
+
+// Выводим результат
+print(sum(first: a, second: b))
